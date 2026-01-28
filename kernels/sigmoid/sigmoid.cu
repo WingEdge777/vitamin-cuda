ff33586f0eb13e6372fef58b802c747c890fe31a
WingEdge777 <handkodu@gmail.com>
2026-01-28 22:39:05 +0800

    update sigmoid : use binding_func_gen

--- kernels/sigmoid/sigmoid.cu
+++ kernels/sigmoid/sigmoid.cu
@@ -6,6 +6,7 @@
 #include <cuda_fp16.h>
 #include <cuda_fp8.h>
 
+#include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 #include <torch/types.h>
 
@@ -25,14 +26,14 @@ __device__ __forceinline__ half2 _sigmoid(half2 x) {
 
 // fp32
 
-__global__ void sigmoid_operator_kernel(float *a, float *b, int N) {
+__global__ void sigmoid_kernel(float *a, float *b, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < N) {
         b[idx] = _sigmoid(a[idx]);
     }
 }
 
-__global__ void sigmoid_operator_fp32x4_kernel(float *a, float *b, int N) {
+__global__ void sigmoid_fp32x4_kernel(float *a, float *b, int N) {
     int idx = 4 * (blockIdx.x * blockDim.x + threadIdx.x);
     float4 a_val = FLOAT4(a[idx]);
     if (idx + 3 < N) {
@@ -48,7 +49,7 @@ __global__ void sigmoid_operator_fp32x4_kernel(float *a, float *b, int N) {
 }
 
 // fp16
-__global__ void sigmoid_operator_kernel(half *a, half *b, int N) {
+__global__ void sigmoid_kernel(half *a, half *b, int N) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < N) {
         b[idx] = _sigmoid(a[idx]);
@@ -56,7 +57,7 @@ __global__ void sigmoid_operator_kernel(half *a, half *b, int N) {
 }
 
 // fp16x2
-__global__ void sigmoid_operator_fp16x2_kernel(half *a, half *b, int N) {
+__global__ void sigmoid_fp16x2_kernel(half *a, half *b, int N) {
     int idx = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
     if (idx + 1 < N) {
         half2 a_val = HALF2(a[idx]);
@@ -67,7 +68,7 @@ __global__ void sigmoid_operator_fp16x2_kernel(half *a, half *b, int N) {
 }
 
 // fp16x8
-__global__ void sigmoid_operator_fp16x8_kernel(half *a, half *b, int N) {
+__global__ void sigmoid_fp16x8_kernel(half *a, half *b, int N) {
     int idx = 8 * (blockIdx.x * blockDim.x + threadIdx.x);
     half2 a_val_0 = HALF2(a[idx]);
     half2 a_val_1 = HALF2(a[idx + 2]);
@@ -89,7 +90,7 @@ __global__ void sigmoid_operator_fp16x8_kernel(half *a, half *b, int N) {
 }
 
 // fp16x8 packed r/w
-__global__ void sigmoid_operator_fp16x8_packed_kernel(half *a, half *b, int N) {
+__global__ void sigmoid_fp16x8_packed_kernel(half *a, half *b, int N) {
     int idx = 8 * (blockIdx.x * blockDim.x + threadIdx.x);
     if (idx + 7 < N) {
         alignas(16) half pack_a[8];
@@ -109,54 +110,46 @@ __global__ void sigmoid_operator_fp16x8_packed_kernel(half *a, half *b, int N) {
         }
     }
 }
+struct Generic {};
 
-template <typename scalar_t>
-void sigmoid_launcher(const torch::Tensor a, torch::Tensor b) {
-    const int total_elements = a.numel();
-    const int threads_per_block = 256;
-    const int blocks_per_grid = (total_elements + threads_per_block - 1) / threads_per_block;
-    sigmoid_operator_kernel<<<blocks_per_grid, threads_per_block>>>(
-        reinterpret_cast<scalar_t *>(a.data_ptr()), reinterpret_cast<scalar_t *>(b.data_ptr()), total_elements);
-}
+#define CHECK_T(x) TORCH_CHECK(x.is_cuda() && x.is_contiguous(), #x " must be contiguous CUDA tensor")
 
-// func for binding
-void sigmoid(torch::Tensor a, torch::Tensor b) {
-    AT_DISPATCH_SWITCH(a.scalar_type(), "sigmoid", AT_DISPATCH_CASE(at::kFloat, [&] {
-                           sigmoid_launcher<float>(a, b);
-                       }) AT_DISPATCH_CASE(at::kHalf, [&] { sigmoid_launcher<half>(a, b); }));
-}
+#define binding_func_gen(name, num, element_dtype)                                                                     \
+    void name(torch::Tensor a, torch::Tensor b) {                                                                      \
+        CHECK_T(a);                                                                                                    \
+        CHECK_T(b);                                                                                                    \
+        const int total_elements = a.numel();                                                                          \
+        const int threads_per_block = 256;                                                                             \
+        const int blocks_per_grid = (total_elements / num + threads_per_block - 1) / threads_per_block;                \
+        cudaStream_t stream = at::cuda::getCurrentCUDAStream();                                                        \
+                                                                                                                       \
+        AT_DISPATCH_FLOATING_TYPES_AND_HALF(                                                                           \
+            b.scalar_type(), #name, ([&] {                                                                             \
+                using namespace std;                                                                                   \
+                using cuda_t = conditional_t<is_same_v<scalar_t, at::Half>, half, scalar_t>;                           \
+                using UserT = conditional_t<is_same_v<element_dtype, Generic>, cuda_t, element_dtype>;                 \
+                using CastT = conditional_t<is_same_v<UserT, double>, float, UserT>;                                   \
+                                                                                                                       \
+                constexpr bool is_generic = is_same_v<element_dtype, Generic>;                                         \
+                constexpr bool is_match = is_same_v<cuda_t, element_dtype>;                                            \
+                constexpr bool is_double = is_same_v<scalar_t, double>;                                                \
+                                                                                                                       \
+                if constexpr (!is_double && (is_generic || is_match)) {                                                \
+                    name##_kernel<<<blocks_per_grid, threads_per_block, 0, stream>>>(                                  \
+                        reinterpret_cast<CastT *>(a.data_ptr()),                                                       \
+                        reinterpret_cast<CastT *>(b.data_ptr()),                                                       \
+                        total_elements);                                                                               \
+                } else {                                                                                               \
+                    TORCH_CHECK(false, #name " does not support " + string(toString(b.scalar_type())));                \
+                }                                                                                                      \
+            }));                                                                                                       \
+    }
 
-void sigmoid_fp32x4(torch::Tensor a, torch::Tensor b) {
-    const int total_elements = a.numel();
-    const int threads_per_block = 256; // keep block thread num equal
-    const int blocks_per_grid = (total_elements / 4 + threads_per_block - 1) / threads_per_block;
-    sigmoid_operator_fp32x4_kernel<<<blocks_per_grid, threads_per_block>>>(
-        reinterpret_cast<float *>(a.data_ptr()), reinterpret_cast<float *>(b.data_ptr()), total_elements);
-}
-
-void sigmoid_fp16x2(torch::Tensor a, torch::Tensor b) {
-    const int total_elements = a.numel();
-    const int threads_per_block = 256; // keep block thread num equal
-    const int blocks_per_grid = (total_elements / 2 + threads_per_block - 1) / threads_per_block;
-    sigmoid_operator_fp16x2_kernel<<<blocks_per_grid, threads_per_block>>>(
-        reinterpret_cast<half *>(a.data_ptr()), reinterpret_cast<half *>(b.data_ptr()), total_elements);
-}
-
-void sigmoid_fp16x8(torch::Tensor a, torch::Tensor b) {
-    const int total_elements = a.numel();
-    const int threads_per_block = 256;
-    const int blocks_per_grid = (total_elements / 8 + threads_per_block - 1) / threads_per_block;
-    sigmoid_operator_fp16x8_kernel<<<blocks_per_grid, threads_per_block>>>(
-        reinterpret_cast<half *>(a.data_ptr()), reinterpret_cast<half *>(b.data_ptr()), total_elements);
-}
-
-void sigmoid_fp16x8_packed(torch::Tensor a, torch::Tensor b) {
-    const int total_elements = a.numel();
-    const int threads_per_block = 256;
-    const int blocks_per_grid = (total_elements / 8 + threads_per_block - 1) / threads_per_block;
-    sigmoid_operator_fp16x8_packed_kernel<<<blocks_per_grid, threads_per_block>>>(
-        reinterpret_cast<half *>(a.data_ptr()), reinterpret_cast<half *>(b.data_ptr()), total_elements);
-}
+binding_func_gen(sigmoid, 1, Generic);
+binding_func_gen(sigmoid_fp32x4, 4, float);
+binding_func_gen(sigmoid_fp16x2, 2, half);
+binding_func_gen(sigmoid_fp16x8, 8, half);
+binding_func_gen(sigmoid_fp16x8_packed, 8, half);
 
 // binding
 #define torch_pybinding_func(f) m.def(#f, &f, #f)
