ff52c7e226f5190f57e9e7f6979d81587272788c
WingEdge777 <handkodu@gmail.com>
2026-02-14 18:29:00 +0800

    add sgemv

---
+++ kernels/sgemv/test.py
@@ -0,0 +1,91 @@
+import time
+from functools import partial
+from typing import Optional
+
+import torch
+from torch.utils.cpp_extension import load
+
+torch.set_grad_enabled(False)
+
+common_flags = ["-O3", "-std=c++17"]
+# Load the CUDA kernel as a python module
+lib = load(
+    name="gemv_lib",
+    sources=["sgemv.cu"],
+    extra_cuda_cflags=common_flags
+    + [
+        "-U__CUDA_NO_HALF_OPERATORS__",
+        "-U__CUDA_NO_HALF_CONVERSIONS__",
+        "-U__CUDA_NO_HALF2_OPERATORS__",
+        "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+        "--expt-relaxed-constexpr",
+        "--expt-extended-lambda",
+        "--use_fast_math",
+    ],
+    extra_cflags=common_flags,
+    verbose=True,
+)
+
+baseline = None
+
+
+def benchmark(op, a, b, c=None, warmup=10, rep=300, prefix="torch"):
+    if c is not None:
+        # warm up
+        for i in range(warmup):
+            op(a, b, c)
+        torch.cuda.synchronize()
+        start = time.perf_counter()
+        for i in range(rep):
+            op(a, b, c)
+        torch.cuda.synchronize()
+    else:
+        # warm up
+        for i in range(warmup):
+            op(a, b)
+        torch.cuda.synchronize()
+        start = time.perf_counter()
+        for i in range(rep):
+            op(a, b)
+        torch.cuda.synchronize()
+
+    duration = time.perf_counter() - start
+
+    if prefix == "torch":
+        global baseline
+        baseline = duration
+        print(f"{prefix:30s} mean time: {duration / rep * 1000:.6f} ms")
+    else:
+        speedup = baseline / duration
+        print(
+            f"{prefix:30s} mean time: {duration / rep * 1000:.6f} ms, speedup: {speedup:.2f}"
+        )
+
+
+def diff_check(a, b, prefix="torch", eps=1e-4):
+    if not torch.allclose(a, b, atol=eps, rtol=eps):
+        print(f"{prefix} result diff: {torch.mean(torch.abs(a - b)).item()}")
+    assert torch.allclose(a, b, atol=eps, rtol=eps), "result diff"
+
+
+if __name__ == "__main__":
+    # test the kernel
+    nm = [512, 1024, 2048, 4096, 8192]
+    torch.manual_seed(42)
+    for n in nm:
+        for m in nm:
+            print("#" * 100)
+            print(f"n: {n}, m: {m}")
+            # a dot b = c
+            a = torch.randn(n, m).float().cuda()
+            b = torch.randn(m, 1).float().cuda()
+            c = torch.zeros(n, 1).cuda()
+
+            benchmark(partial(torch.matmul, out=c), a, b)
+            c_my = torch.zeros_like(c)
+
+            benchmark(lib.gemv, a, b, c_my, prefix="gemv")
+            diff_check(c, c_my, prefix="gemv")
+            benchmark(lib.gemv_fp32x4, a, b, c_my, prefix="gemv_fp32x4")
+            # print(c.flatten(), c_my.flatten())
+            diff_check(c, c_my, prefix="gemv_fp32x4")
