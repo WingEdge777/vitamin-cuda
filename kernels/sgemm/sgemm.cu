040fdff8660c5f0783cb6ff43cf63c5750e3bc91
WingEdge777 <handkodu@gmail.com>
2026-02-24 21:34:09 +0800

    update sgemm

--- kernels/sgemm/sgemm.cu
+++ kernels/sgemm/sgemm.cu
@@ -13,40 +13,8 @@
 #define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
 #define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
 #define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
-#define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
 
 const int WARP_SIZE = 32;
-
-template <const int warp_size = WARP_SIZE>
-__device__ __forceinline__ float _warp_shuffle_reduce_sum(float val) {
-#pragma unroll
-    for (int offset = warp_size >> 1; offset > 0; offset >>= 1) {
-        val += __shfl_xor_sync(0xffffffff, val, offset);
-    }
-    return val;
-}
-
-template <const int num_warp = 8>
-__device__ __forceinline__ float _block_reduce_sum(float val) {
-    val = _warp_shuffle_reduce_sum<WARP_SIZE>(val);
-
-    __shared__ float sdata[num_warp];
-    int lane_id = threadIdx.x % WARP_SIZE;
-    int warp_id = threadIdx.x / WARP_SIZE;
-    if (lane_id == 0) {
-        sdata[warp_id] = val;
-    }
-    __syncthreads();
-    if (warp_id == 0) {
-        val = lane_id < num_warp ? sdata[lane_id] : 0.f;
-        val = _warp_shuffle_reduce_sum<num_warp>(val);
-        if (lane_id == 0) {
-            sdata[0] = val;
-        }
-    }
-    __syncthreads();
-    return sdata[0];
-}
 
 // gemm fp32
 __global__ void sgemm_naive_kernel(float *a, float *b, float *c, int m, int n, int k) {
@@ -62,13 +30,71 @@ __global__ void sgemm_naive_kernel(float *a, float *b, float *c, int m, int n, i
     }
 }
 
-template <const int BM, const BN, const int BK, const int TM, const int TN>
-__global__ void sgemm_kernel(float *a, float *b, float *c, int m, int n, int k) {}
+// a block calculate c[128][128], each thread c[8][8]
+template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
+__global__ void sgemm_kernel(float *a, float *b, float *c, int m, int n, int k) {
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tid = threadIdx.x; // 0~255; 8 个 warp, 2x4 tiling; 每个warp 4*8 tiling
+    int warp_id = tid / 32;
+    int lane_id = tid % 32;
 
-template <const int BM, const BN, const int BK, const int TM, const int TN>
+    // 一个block一次搬运 64x16个a， 8x128个b， 分两次搬运
+    // 每4个线程负责一行a(16个元素)，每32个线程负责一行b(128个元素)
+    int load_a_row = tid / 4;
+    int load_a_col = (tid % 4) * 4;
+    int load_b_row = tid / 32;
+    int load_b_col = (tid % 32) * 4;
+
+    // warp tiling
+    int warp_row = warp_id / 4;      // 0, 1
+    int warp_col = warp_id % 4;      // 0, 1, 2, 3
+    int t_row_in_warp = lane_id / 4; // 0~7
+    int t_col_in_warp = lane_id % 4; // 0~3
+
+    // c out 初始坐标， 每个线程负责 8x8 个元素
+    int c_row = warp_row * 64 + t_row_in_warp * 8;
+    int c_col = warp_col * 32 + t_col_in_warp * 8;
+
+    __shared__ float As[BM][BK]; // 128x16
+    __shared__ float Bs[BK][BN]; // 16x128
+    float sum[TM][TN] = {0.f};
+    for (int bk = 0; bk < k; bk += BK) {
+        FLOAT4(As[load_a_row][load_a_col]) = FLOAT4(a[(by * BM + load_a_row) * k + bk + load_a_col]);
+        FLOAT4(As[load_a_row + 64][load_a_col]) = FLOAT4(a[(by * BM + load_a_row + 64) * k + bk + load_a_col]);
+        FLOAT4(Bs[load_b_row][load_b_col]) = FLOAT4(b[(bk + load_b_row) * n + bx * BN + load_b_col]);
+        FLOAT4(Bs[load_b_row + 8][load_b_col]) = FLOAT4(b[(bk + load_b_row + 8) * n + bx * BN + load_b_col]);
+        __syncthreads();
+#pragma unroll
+        for (int i = 0; i < BK; i++) {
+            float reg_a[TM], reg_b[TN];
+#pragma unroll
+            for (int j = 0; j < TM; j++)
+                reg_a[j] = As[(c_row + j)][i];
+            FLOAT4(reg_b[0]) = FLOAT4(Bs[i][c_col]);
+            FLOAT4(reg_b[4]) = FLOAT4(Bs[i][c_col + 4]);
+
+#pragma unroll
+            for (int m_idx = 0; m_idx < TM; ++m_idx) {
+#pragma unroll
+                for (int n_idx = 0; n_idx < TN; ++n_idx) {
+                    sum[m_idx][n_idx] += reg_a[m_idx] * reg_b[n_idx];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+#pragma unroll
+    for (int i = 0; i < TM; ++i) {
+        FLOAT4(c[(by * BM + c_row + i) * n + bx * BN + c_col]) = FLOAT4(sum[i][0]);
+        FLOAT4(c[(by * BM + c_row + i) * n + bx * BN + c_col + 4]) = FLOAT4(sum[i][4]);
+    }
+}
+
+template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
 __global__ void sgemm_bcf_kernel(float *a, float *b, float *c, int m, int n, int k) {}
 
-template <const int BM, const BN, const int BK, const int TM, const int TN>
+template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
 __global__ void sgemm_bcf_dbf_kernel(float *a, float *b, float *c, int m, int n, int k) {}
 
 #define CHECK_T(x) TORCH_CHECK(x.is_cuda() && x.is_contiguous(), #x " must be contiguous CUDA tensor")
@@ -89,9 +115,28 @@ __global__ void sgemm_bcf_dbf_kernel(float *a, float *b, float *c, int m, int n,
             a.data_ptr<float>(), b.data_ptr<float>(), c.data_ptr<float>(), M, N, K);                                   \
     }
 
+#define binding_tiled_func_gen(name)                                                                                   \
+    void name(torch::Tensor a, torch::Tensor b, torch::Tensor c) {                                                     \
+        CHECK_T(a);                                                                                                    \
+        CHECK_T(b);                                                                                                    \
+        CHECK_T(c);                                                                                                    \
+        const int M = a.size(0);                                                                                       \
+        const int K = a.size(1);                                                                                       \
+        const int N = b.size(1);                                                                                       \
+        const int BM = 128;                                                                                            \
+        const int BN = 128;                                                                                            \
+        const int threads_per_block = 256;                                                                             \
+        const dim3 blocks_per_grid((N + BN - 1) / BN, (M + BM - 1) / BM);                                              \
+        cudaStream_t stream = at::cuda::getCurrentCUDAStream();                                                        \
+                                                                                                                       \
+        name##_kernel<128, 128, 16, 8, 8><<<blocks_per_grid, threads_per_block, 0, stream>>>(                          \
+            a.data_ptr<float>(), b.data_ptr<float>(), c.data_ptr<float>(), M, N, K);                                   \
+    }
+
 extern void sgemm_cublas(torch::Tensor a, torch::Tensor b, torch::Tensor c);
 extern void sgemm_cublas_tf32(torch::Tensor a, torch::Tensor b, torch::Tensor c);
 binding_func_gen(sgemm_naive, 1, float);
+binding_tiled_func_gen(sgemm);
 
 // binding
 #define torch_pybinding_func(f) m.def(#f, &f, #f)
@@ -100,4 +145,5 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     torch_pybinding_func(sgemm_cublas);
     torch_pybinding_func(sgemm_cublas_tf32);
     torch_pybinding_func(sgemm_naive);
+    torch_pybinding_func(sgemm);
 }
