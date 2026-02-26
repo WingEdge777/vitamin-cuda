067d9ebadb730f2d0381a4fa455d0b20730be8e4
WingEdge777 <handkodu@gmail.com>
2026-02-26 14:20:42 +0800

    update sgemm : add swizzling

--- kernels/sgemm/sgemm.cu
+++ kernels/sgemm/sgemm.cu
@@ -14,6 +14,8 @@
 #define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
 #define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
 
+#define SWIZZLE_A(k, m) ((m) ^ (((m) >> 5) << 2) ^ (((k) >> 2) << 3))
+
 const int WARP_SIZE = 32;
 
 // gemm fp32
@@ -32,7 +34,7 @@ __global__ void sgemm_naive_kernel(float *a, float *b, float *c, int m, int n, i
 
 // a block calculate c[128][128], each thread c[8][8]
 template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
-__global__ void sgemm_kernel(float *a, float *b, float *c, int m, int n, int k) {
+__global__ void sgemm_tiling_kernel(float *a, float *b, float *c, int m, int n, int k) {
     int bx = blockIdx.x, by = blockIdx.y;
     int tid = threadIdx.x; // 0~255; 8 个 warp, 2x4 tiling; 每个warp 8x4 tiling
     int warp_id = tid / WARP_SIZE;
@@ -110,7 +112,7 @@ __global__ void sgemm_kernel(float *a, float *b, float *c, int m, int n, int k) 
 }
 
 template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
-__global__ void sgemm_bcf_kernel(float *a, float *b, float *c, int m, int n, int k) {
+__global__ void sgemm_padding_kernel(float *a, float *b, float *c, int m, int n, int k) {
     int bx = blockIdx.x, by = blockIdx.y;
     int tid = threadIdx.x; // 0~255; 8 个 warp, 2x4 tiling; 每个warp 8x4 tiling
     int warp_id = tid / WARP_SIZE;
@@ -135,7 +137,7 @@ __global__ void sgemm_bcf_kernel(float *a, float *b, float *c, int m, int n, int
 
     // padding 4 字节
     __shared__ float As_T[BK][BM + 4];
-    __shared__ float Bs[BK][BN + 4];
+    __shared__ float Bs[BK][BN];
 
     float sum[TM][TN] = {0.f};
 
@@ -165,6 +167,86 @@ __global__ void sgemm_bcf_kernel(float *a, float *b, float *c, int m, int n, int
 
             FLOAT4(reg_a[0]) = FLOAT4(As_T[i][c_row]);
             FLOAT4(reg_a[4]) = FLOAT4(As_T[i][c_row + 4]);
+
+            FLOAT4(reg_b[0]) = FLOAT4(Bs[i][c_col]);
+            FLOAT4(reg_b[4]) = FLOAT4(Bs[i][c_col + 4]);
+
+#pragma unroll
+            for (int m_idx = 0; m_idx < TM; ++m_idx) {
+#pragma unroll
+                for (int n_idx = 0; n_idx < TN; ++n_idx) {
+                    sum[m_idx][n_idx] += reg_a[m_idx] * reg_b[n_idx];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    // 写回 C 矩阵
+#pragma unroll
+    for (int i = 0; i < TM; ++i) {
+        FLOAT4(c[(by * BM + c_row + i) * n + bx * BN + c_col]) = FLOAT4(sum[i][0]);
+        FLOAT4(c[(by * BM + c_row + i) * n + bx * BN + c_col + 4]) = FLOAT4(sum[i][4]);
+    }
+}
+
+//
+template <const int BM = 128, const int BN = 128, const int BK = 16, const int TM = 8, const int TN = 8>
+__global__ void sgemm_bcf_swizzling_kernel(float *a, float *b, float *c, int m, int n, int k) {
+    int bx = blockIdx.x, by = blockIdx.y;
+    int tid = threadIdx.x; // 0~255; 8 个 warp, 2x4 tiling; 每个warp 8x4 tiling
+    int warp_id = tid / WARP_SIZE;
+    int lane_id = tid % WARP_SIZE;
+
+    // 一个block一次搬运 64x16个a， 8x128个b， 分两次搬运
+    // 每4个线程负责一行a(16个元素)，每32个线程负责一行b(128个元素)
+    int load_a_row = tid / 4;
+    int load_a_col = (tid % 4) * 4;
+    int load_b_row = tid / WARP_SIZE;
+    int load_b_col = (tid % WARP_SIZE) * 4;
+
+    // warp tiling
+    int warp_row = warp_id / 4;      // 0, 1
+    int warp_col = warp_id % 4;      // 0, 1, 2, 3
+    int t_row_in_warp = lane_id / 4; // 0~7
+    int t_col_in_warp = lane_id % 4; // 0~3
+
+    // c out 初始坐标， 每个线程负责 8行8列 tile, 共256线程，256*64 = 128*128
+    int c_row = warp_row * 64 + t_row_in_warp * 8;
+    int c_col = warp_col * 32 + t_col_in_warp * 8;
+
+    // padding 4 字节
+    __shared__ float As_T[BK][BM];
+    __shared__ float Bs[BK][BN];
+
+    float sum[TM][TN] = {0.f};
+
+    for (int bk = 0; bk < k; bk += BK) {
+        // A 矩阵转置写入共享内存
+        float4 tmp_a0 = FLOAT4(a[(by * BM + load_a_row) * k + bk + load_a_col]);
+        As_T[load_a_col + 0][SWIZZLE_A(load_a_col + 0, load_a_row)] = tmp_a0.x;
+        As_T[load_a_col + 1][SWIZZLE_A(load_a_col + 1, load_a_row)] = tmp_a0.y;
+        As_T[load_a_col + 2][SWIZZLE_A(load_a_col + 2, load_a_row)] = tmp_a0.z;
+        As_T[load_a_col + 3][SWIZZLE_A(load_a_col + 3, load_a_row)] = tmp_a0.w;
+
+        float4 tmp_a1 = FLOAT4(a[(by * BM + load_a_row + 64) * k + bk + load_a_col]);
+        As_T[load_a_col + 0][SWIZZLE_A(load_a_col + 0, load_a_row + 64)] = tmp_a1.x;
+        As_T[load_a_col + 1][SWIZZLE_A(load_a_col + 1, load_a_row + 64)] = tmp_a1.y;
+        As_T[load_a_col + 2][SWIZZLE_A(load_a_col + 2, load_a_row + 64)] = tmp_a1.z;
+        As_T[load_a_col + 3][SWIZZLE_A(load_a_col + 3, load_a_row + 64)] = tmp_a1.w;
+
+        FLOAT4(Bs[load_b_row][load_b_col]) = FLOAT4(b[(bk + load_b_row) * n + bx * BN + load_b_col]);
+        FLOAT4(Bs[load_b_row + 8][load_b_col]) = FLOAT4(b[(bk + load_b_row + 8) * n + bx * BN + load_b_col]);
+
+        __syncthreads();
+
+        // 8x8循环计算累加乘积和
+#pragma unroll
+        for (int i = 0; i < BK; i++) {
+            float reg_a[TM], reg_b[TN];
+
+            FLOAT4(reg_a[0]) = FLOAT4(As_T[i][SWIZZLE_A(i, c_row)]);
+            FLOAT4(reg_a[4]) = FLOAT4(As_T[i][SWIZZLE_A(i, c_row + 4)]);
 
             FLOAT4(reg_b[0]) = FLOAT4(Bs[i][c_col]);
             FLOAT4(reg_b[4]) = FLOAT4(Bs[i][c_col + 4]);
@@ -229,9 +311,12 @@ __global__ void sgemm_bcf_dbf_kernel(float *a, float *b, float *c, int m, int n,
 
 extern void sgemm_cublas(torch::Tensor a, torch::Tensor b, torch::Tensor c);
 extern void sgemm_cublas_tf32(torch::Tensor a, torch::Tensor b, torch::Tensor c);
+
 binding_func_gen(sgemm_naive, 1, float);
-binding_tiled_func_gen(sgemm);
-binding_tiled_func_gen(sgemm_bcf);
+
+binding_tiled_func_gen(sgemm_tiling);
+binding_tiled_func_gen(sgemm_padding);
+binding_tiled_func_gen(sgemm_bcf_swizzling);
 
 // binding
 #define torch_pybinding_func(f) m.def(#f, &f, #f)
@@ -240,6 +325,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     torch_pybinding_func(sgemm_cublas);
     torch_pybinding_func(sgemm_cublas_tf32);
     torch_pybinding_func(sgemm_naive);
-    torch_pybinding_func(sgemm);
-    torch_pybinding_func(sgemm_bcf);
+    torch_pybinding_func(sgemm_tiling);
+    torch_pybinding_func(sgemm_padding);
+    torch_pybinding_func(sgemm_bcf_swizzling);
 }
