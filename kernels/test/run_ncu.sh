1f6aabf6c4b228cdd89e676187c79e8bd5cc01b5
WingEdge777 <handkodu@gmail.com>
2026-03-03 12:45:28 +0800

    add test

---
+++ kernels/test/run_ncu.sh
@@ -0,0 +1,1 @@
+ncu -k regex:"load" --set full -o profile_res -f python test_for_ncu.py
---
+++ kernels/test/test_for_ncu.py
@@ -0,0 +1,35 @@
+import time
+from functools import partial
+from typing import Optional
+
+import torch
+from torch.utils.cpp_extension import load
+
+torch.set_grad_enabled(False)
+
+common_flags = ["-O3", "-std=c++17"]
+# Load the CUDA kernel as a python module
+lib = load(
+    name="load_lib",
+    sources=["test.cu"],
+    extra_cuda_cflags=common_flags
+    + [
+        "-U__CUDA_NO_HALF_OPERATORS__",
+        "-U__CUDA_NO_HALF_CONVERSIONS__",
+        "-U__CUDA_NO_HALF2_OPERATORS__",
+        "-U__CUDA_NO_BFLOAT16_CONVERSIONS__",
+        "--expt-relaxed-constexpr",
+        "--expt-extended-lambda",
+        "--use_fast_math",
+    ],
+    extra_cflags=common_flags,
+    verbose=True,
+)
+
+
+if __name__ == "__main__":
+    # test the kernel
+    a = torch.randn(128).float().cuda()
+    b = torch.zeros_like(a)
+    lib.load_fp32x4(a, b)
+    print(b)
