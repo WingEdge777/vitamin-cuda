6ce8506127710478f1ec6eee4ff20722ddfbcc59
WingEdge777 <handkodu@gmail.com>
2026-04-06 11:28:37 +0800

    update common

--- kernels/common/pack.cuh
+++ kernels/common/pack.cuh
@@ -1,6 +1,9 @@
 #pragma once
+#include <cstdint>
+
 #include <cuda_bf16.h>
 #include <cuda_fp16.h>
+#include <vector_types.h>
 
 #define UINT2(value) (reinterpret_cast<uint2 *>(&(value))[0])
 #define FLOAT2(value) (reinterpret_cast<float2 *>(&(value))[0])
@@ -12,6 +15,8 @@
 union alignas(16) pack128 {
     float4 f4;
     float f[4];
+    int4 i4;
+    int8_t i8[16];
     half2 h2[4];
     half h[8];
     __nv_bfloat162 bf2[4];
